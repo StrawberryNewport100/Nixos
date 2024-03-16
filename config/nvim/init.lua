6fc05a6e11d60116381ae052c05263c53885b101
Newport <Cigarettellos@proton.me>
2024-03-16 15:14:16 -0400

    changed clock, changed nvim indents

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -7,6 +7,7 @@ require('plugin.packer')
 require('plugin.treesitter')
 require('plugin.lsp')
 require('plugin.telescope')
+require('plugin.indent-blankline')
 require('terminal')
 require('theme')
 --Let me know i've sourced this file
